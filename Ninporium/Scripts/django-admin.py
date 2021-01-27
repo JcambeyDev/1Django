ad00ae5e43241c56cec9f5dec282ee9f998881d0
Jorge <jcambeyp@gmail.com>
2021-01-27 11:53:11 +0000

    Meu primeiro commit bros

---
+++ Ninporium/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jorge cambey\google drive\fcup\ninpo\ninporium\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
